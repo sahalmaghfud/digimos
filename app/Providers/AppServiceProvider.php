1c43ad64bfb3837d243f258c372b33173cf81fa3
ucupican <ikhsansage@gmail.com>
2024-12-12 15:33:33 +0700

    language

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
             $switch
-                ->locales(['id']);
+                ->locales(['id', 'en']);
         });
 
         Blade::component('mobile-menu', MobileMenu::class);
